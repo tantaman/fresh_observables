39ca00d64e8b61514da68c2cc81f8bc0d166014b
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2012-05-30 00:29:53 -0400

    Refreshed my memory on Javassist and added the main methods needed in ObservablesTransformer

---
+++ src/main/java/com/tantaman/fresh_observables/annotations/Observable.java
@@ -0,0 +1,8 @@
+package com.tantaman.fresh_observables.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Observable {
+}
--- src/main/java/com/tantaman/fresh_observables/transformers/ObservablesTransformer.java
+++ src/main/java/com/tantaman/fresh_observables/transformers/ObservablesTransformer.java
@@ -1,14 +1,78 @@
 package com.tantaman.fresh_observables.transformers;
 
+import java.io.ByteArrayInputStream;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
-public class ObservablesTransformer implements ClassFileTransformer {
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.NotFoundException;
 
-	public byte[] transform(ClassLoader arg0, String arg1, Class<?> arg2,
-			ProtectionDomain arg3, byte[] arg4)
+public class ObservablesTransformer implements ClassFileTransformer {
+	private ClassPool classPool = ClassPool.getDefault();
+	
+	public byte[] transform(ClassLoader loader, String className, Class<?> clazz,
+			ProtectionDomain protectionDomain, byte[] classfileBuffer)
 			throws IllegalClassFormatException {
-		return null;
+		CtClass cl = null;
+		byte [] result = null;
+		
+		try {
+			// TODO: is this quick enough or is it quicker to do "new ClassFile" and check its inheritance tree there?
+			cl = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+			
+			if (shouldInstrument(cl)) {
+				boolean instrumented = instrument(cl);
+				if (instrumented)
+					result = cl.toBytecode();
+			}
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		
+		if (result != null) {
+			return result;
+		} else {
+			return classfileBuffer;
+		}
+	}
+	
+	private boolean shouldInstrument(CtClass cl) {
+		try {
+			for (CtClass iface : cl.getInterfaces()) {
+				if (iface.getName().equals("com.tantaman.fresh_observables.Observable"))
+					return true;
+			}
+		} catch (NotFoundException e) {
+			return false;
+		}
+		return false;
+	}
+	
+	private boolean instrument(CtClass cl) {
+		boolean result = false;
+		for (CtMethod method : cl.getMethods()) {
+			result = result || (processMethod(method));
+		}
+		return result;
+	}
+	
+	private boolean processMethod(CtMethod method) {
+		try {
+			for (Object annotation : method.getAnnotations()) {
+				/*String annotationAsString =
+						(String)(annotation.getClass().getMethod("toString").invoke(annotation));
+					String annotationName = WrappedAnnotation.determineClassName(annotationAsString);*/
+				/*
+				 * Or . . . 
+				 */
+			}
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
+		
+		return false;
 	}
 }
